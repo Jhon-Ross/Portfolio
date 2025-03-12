cc2404cfb809a9ab6d4ea92736e9cdcdf6588294
Jhon Ross <jhonross.lp@outlook.com>
2025-03-12 16:29:57 -0300

    Corrigindo server.js

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
-
 
 // Servir arquivos estáticos da raiz e da pasta "src"
 app.use(express.static(__dirname));
