17d12be40f93d3b42cb011b95ca0bbf14fea99d3
Jhon Ross <jhonross.lp@outlook.com>
2025-03-13 12:20:33 -0300

    setando chave privada

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ from flask import Flask, request, jsonify, render_template
 import os
 from dotenv import load_dotenv
 import requests
+import logging
 
 load_dotenv()
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
+app.logger.setLevel(logging.DEBUG)
 
 EMAILJS_SERVICE_ID = os.environ.get('EMAILJS_SERVICE_ID')
 EMAILJS_TEMPLATE_ID = os.environ.get('EMAILJS_TEMPLATE_ID')
-EMAILJS_PUBLIC_KEY = os.environ.get('EMAILJS_PUBLIC_KEY')
+EMAILJS_PRIVATE_KEY = os.environ.get(
+    'EMAILJS_PRIVATE_KEY')  # Use a chave privada
 
 
 @app.route('/', methods=['GET'])
@@ -24,11 +27,10 @@ def enviar_email():
         email = request.form['email']
         mensagem = request.form['mensagem']
 
-        # Construa o payload para a API do EmailJS
         payload = {
             'service_id': EMAILJS_SERVICE_ID,
             'template_id': EMAILJS_TEMPLATE_ID,
-            'user_id': EMAILJS_PUBLIC_KEY,
+            'user_id': EMAILJS_PRIVATE_KEY,  # Use a chave privada
             'template_params': {
                 'nome': nome,
                 'email': email,
@@ -36,19 +38,21 @@ def enviar_email():
             }
         }
 
+        app.logger.debug(f"Payload: {payload}")
+
         try:
-            # Envie a requisição POST para a API do EmailJS
             response = requests.post(
                 'https://api.emailjs.com/api/v1.0/email/send', json=payload)
+            app.logger.debug(
+                f"Response Status Code: {response.status_code}, Response Text: {response.text}")
 
-            # Verifique se a requisição foi bem-sucedida
             if response.status_code == 200:
                 return jsonify({'success': True, 'message': 'E-mail enviado com sucesso!'})
             else:
                 return jsonify({'success': False, 'message': f'Erro ao enviar e-mail: {response.text}'})
 
         except Exception as e:
-            print(e)
+            app.logger.error(f"Erro ao enviar e-mail: {str(e)}")
             return jsonify({'success': False, 'message': f'Erro ao enviar e-mail: {str(e)}'})
     else:
         return jsonify({'success': False, 'message': 'Método não permitido!'})
